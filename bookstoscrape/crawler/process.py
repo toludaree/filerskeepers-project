db3dbbf0c19bcf98c15f19340197a5219c2e2006
toludaree <isaactoluwani30@gmail.com>
2025-11-05 23:41:48 +0100

    add function structure and tests for process_book

--- bookstoscrape/crawler/process.py
+++ bookstoscrape/crawler/process.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+from .models import Book
 
-def process_page(page: bytes, page_url: str):
+
+def process_page(page: bytes, page_url: str) -> list[str]:
     """
-    Retrieve all the book urls on a given page
+    Retrieve all the book urls on a given page.
     """
     soup = BeautifulSoup(page, "html.parser")
     books_tag = soup.find_all(name="article", class_="product_pod")
@@ -13,3 +15,21 @@ def process_page(page: bytes, page_url: str):
         for tag in books_tag
     ]
     return books_url
+
+def process_book(content: bytes, book_url: str) -> Book:
+    """
+    Process book page and return a Book pydantic model.
+    """
+    return Book(
+        id=0,
+        name="",
+        description="",
+        category="",
+        price_including_tax=0.0,
+        price_excluding_tax=0.0,
+        in_stock=False,
+        stock_count=0,
+        review_count=0,
+        cover_image_url="https://cover_image.com",
+        rating=1
+    )
--- bookstoscrape/tests/test.py
+++ bookstoscrape/tests/test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from ..crawler.process import process_page
+from ..crawler.models import Book
+from ..crawler.process import process_page, process_book
 
 
 class TestProcess(unittest.TestCase):
@@ -60,4 +61,59 @@ class TestProcess(unittest.TestCase):
         self.assertEqual(process_page(page1, "https://books.toscrape.com/"), self.page1_urls)
         self.assertEqual(process_page(page4, "https://books.toscrape.com/catalogue/page-4.html"), self.page4_urls)
 
+    def test__process_book(self):
+        with open("./bookstoscrape/tests/book1.html", "rb") as f1:
+            book1 = f1.read()
+        with open("./bookstoscrape/tests/book2.html", "rb") as f2:
+            book2 = f2.read()
+        with open("./bookstoscrape/tests/book3.html", "rb") as f3:
+            book3 = f3.read()
 
+        self.assertEqual(
+            process_book(book1, "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"),
+            Book(
+                id=1000,
+                name="A Light in the Attic",
+                description="It's hard to imagine a world without A Light in the Attic. This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th anniversary with this special edition. Silverstein's humorous and creative verse can amuse the dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic words and laugh and smile and love th It's hard to imagine a world without A Light in the Attic. This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th anniversary with this special edition. Silverstein's humorous and creative verse can amuse the dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic words and laugh and smile and love that Silverstein. Need proof of his genius? RockabyeRockabye baby, in the treetopDon't you know a treetopIs no safe place to rock?And who put you up there,And your cradle, too?Baby, I think someone down here'sGot it in for you. Shel, you never sounded so good. ...more",
+                category="Poetry",
+                price_including_tax=51.77,
+                price_excluding_tax=51.77,
+                in_stock=True,
+                stock_count=22,
+                review_count=0,
+                cover_image_url="https://books.toscrape.com/media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg",
+                rating=3
+            )
+        )
+        self.assertEqual(
+            process_book(book2, "https://books.toscrape.com/catalogue/angels-walking-angels-walking-1_662/index.html"),
+            Book(
+                id=662,
+                name="Angels Walking (Angels Walking #1)",
+                description="When former national baseball star Tyler Ames suffers a career-ending injury, all he can think about is putting his life back together the way it was before. He has lost everyone he loves on his way to the big leagues. Then just when things seem to be turning around, Tyler hits rock bottom. Across the country, Tyler’s one true love Sami Dawson has moved on. A series of sma When former national baseball star Tyler Ames suffers a career-ending injury, all he can think about is putting his life back together the way it was before. He has lost everyone he loves on his way to the big leagues. Then just when things seem to be turning around, Tyler hits rock bottom. Across the country, Tyler’s one true love Sami Dawson has moved on. A series of small miracles leads Tyler to a maintenance job at a retirement home and a friendship with Virginia Hutcheson, an old woman with Alzheimer’s who strangely might have the answers he so desperately seeks.A team of Angels Walking take on the mission to restore hope for Tyler, Sami, and Virginia. Can such small and seemingly insignificant actions of the unseen bring healing and redemption? And can the words of a stranger rekindle lost love? Every journey begins with a step.It is time for the mission to begin… ...more",
+                category="Add a comment",
+                price_including_tax=34.20,
+                price_excluding_tax=34.20,
+                in_stock=True,
+                stock_count=14,
+                review_count=0,
+                cover_image_url="https://books.toscrape.com/media/cache/ba/d9/bad95369105e8e403bf1f2b9288c5e41.jpg",
+                rating=2
+            )
+        )
+        self.assertEqual(
+            process_book(book3, "https://books.toscrape.com/catalogue/the-lonely-city-adventures-in-the-art-of-being-alone_639/index.html"),
+            Book(
+                id=639,
+                name="The Lonely City: Adventures in the Art of Being Alone",
+                description="An expertly crafted work of reportage, memoir and biography on the subject of loneliness told through the lives of iconic artists, by the acclaimed author of The Trip to Echo Spring What does it mean to be lonely? How do we live, if we're not intimately engaged with another human being? How do we connect with other people? Does technology draw us closer together or trap us An expertly crafted work of reportage, memoir and biography on the subject of loneliness told through the lives of iconic artists, by the acclaimed author of The Trip to Echo Spring What does it mean to be lonely? How do we live, if we're not intimately engaged with another human being? How do we connect with other people? Does technology draw us closer together or trap us behind screens?When Olivia Laing moved to New York City in her mid-thirties, she found herself inhabiting loneliness on a daily basis. Increasingly fascinated by this most shameful of experiences, she began to explore the lonely city by way of art. Moving fluidly between works and lives - from Edward Hopper's Nighthawks to Andy Warhol's Time Capsules, from Henry Darger's hoarding to the depredations of the AIDS crisis - Laing conducts an electric, dazzling investigation into what it means to be alone, illuminating not only the causes of loneliness but also how it might be resisted and redeemed.Humane, provocative and deeply moving, The Lonely City is about the spaces between people and the things that draw them together, about sexuality, mortality and the magical possibilities of art. It's a celebration of a strange and lovely state, adrift from the larger continent of human experience, but intrinsic to the very act of being alive. ...more",
+                category="Nonfiction",
+                price_including_tax=33.26,
+                price_excludign_tax=33.26,
+                in_stock=True,
+                stock_count=12,
+                review_count=0,
+                cover_image_url="https://books.toscrape.com/media/cache/79/66/79660d2683a90670aa014cae6e02b2dc.jpg",
+                rating=2
+            )
+        )
