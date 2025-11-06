ff6f0dc1d75e850e395fe2a48d10be6e6d129757
toludaree <isaactoluwani30@gmail.com>
2025-11-06 10:49:57 +0100

    proposed approach for fetch module

---
+++ bookstoscrape/crawler/fetch.py
@@ -0,0 +1,66 @@
+import asyncio
+from httpx import AsyncClient
+
+from .models import Book
+from .process import process_page, process_book
+
+
+page_headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "sec-ch-ua": '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "naviagate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+}
+book_headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "referer": "https://books.toscrape.com/catalogue/page-1.html",
+    "sec-ch-ua": '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+}
+
+async def fetch_page(client: AsyncClient, page_url: str) -> list[str]:
+    page = await client.get(
+        url=page_url,
+        headers=page_headers
+    )
+    page.raise_for_status()
+    
+    urls = await asyncio.to_thread(
+        process_page, page.content, page_url
+    )
+    return urls
+
+async def fetch_book(client: AsyncClient, book_url: str) -> Book:
+    book_page = await client.get(
+        url=book_url,
+        headers=book_headers
+    )
+    book_page.raise_for_status()
+
+    book = await asyncio.to_thread(
+        process_book, book_page, book_url
+    )
+    return book
