329af2bcccfebce65c31674c972b94384cfdc419
toludaree <isaactoluwani30@gmail.com>
2025-11-07 21:29:52 +0100

    add exception handling and retry logic

--- bookstoscrape/crawler/main.py
+++ bookstoscrape/crawler/main.py
@@ -1,6 +1,6 @@
 import asyncio
 import math
-from httpx import AsyncClient
+from httpx import AsyncClient, RequestError
 
 from . import settings
 from .fetch import fetch_page, fetch_book
@@ -10,9 +10,10 @@ from .utils import extract_id_from_book_url
 
 async def bookstoscrape_crawler(
     worker_count=5,
+    max_retry_count=3,
     env="dev"
 ):
-    manager = Manager(env=env)
+    manager = Manager(env=env, max_retry_count=max_retry_count)
     await manager.queue.put(PageSession(
         sid=1, page_num=1,
         page_url=f"{settings.BASE_URL}/page-1.html",
@@ -34,6 +35,7 @@ async def bookstoscrape_crawler(
 
 async def worker(wid: int, manager: Manager):
     while True:
+        session = None
         try:
             session: PageSession | BookSession = await manager.queue.get()
             if not session.retry_count:
@@ -43,25 +45,43 @@ async def worker(wid: int, manager: Manager):
             manager.logger.info(f"[{wid}][{session.stype}][{session.sid}] 👨‍🍳 {message}")
 
             async with AsyncClient(follow_redirects=True, proxy=settings.PROXY, timeout=settings.REQUEST_TIMEOUT) as client:
-                if isinstance(session, PageSession):
-                    book_count, book_urls = await fetch_page(client, session.page_url)
-                    if session.first_page:
-                        page_count = math.ceil(book_count / len(book_urls))
-                        for i in range(2, page_count+1):
-                            await manager.queue.put(PageSession(
-                                sid=i, page_num=i,
-                                page_url=f"{settings.BASE_URL}/page-{i}.html"
+                try:
+                    if isinstance(session, PageSession):
+                        book_count, book_urls = await fetch_page(client, session.page_url)
+                        if session.first_page:
+                            page_count = math.ceil(book_count / len(book_urls))
+                            for i in range(2, page_count+1):
+                                await manager.queue.put(PageSession(
+                                    sid=i, page_num=i,
+                                    page_url=f"{settings.BASE_URL}/page-{i}.html"
+                                ))
+                        for url in book_urls:
+                            await manager.queue.put(BookSession(
+                                sid=extract_id_from_book_url(url),
+                                book_url=url
                             ))
-                    for url in book_urls:
-                        await manager.queue.put(BookSession(
-                            sid=extract_id_from_book_url(url),
-                            book_url=url
-                        ))
-                else:
-                    book = await fetch_book(
-                        client,
-                        session.sid, session.book_url
-                    )
-
-        except:
-            ...
+                    else:
+                        book = await fetch_book(
+                            client,
+                            session.sid, session.book_url
+                        )
+                        manager.cleanup(book, session.sid)
+                except RequestError as exc:
+                    manager.logger.warning(f"[{wid}][{session.stype}][{session.sid}] ❌ {repr(exc)}")
+                    if session.retry_count < manager.max_retry_count:
+                        session.retry_count += 1
+                        await manager.queue.put(session)
+                        manager.logger.info(f"[{wid}][{session.stype}][{session.sid}] 🤞 Queued for retry")
+                    else:
+                        manager.logger.info(f"[manager][{session.stype}][{session.sid}] 😑 Retry limit reached")
+                    continue
+                
+        except asyncio.CancelledError:
+            manager.logger.info(f"[manager] 😎 Worker {wid} stopped")
+            break
+        except Exception as exc:
+            sid = f"[{session.stype}][{session.sid}]" if session else ""
+            manager.logger.exception(f"[{wid}]{sid} ❌ {repr(exc)}")
+        finally:
+            if session:
+                manager.queue.task_done()
--- bookstoscrape/crawler/manager.py
+++ bookstoscrape/crawler/manager.py
@@ -26,15 +26,17 @@ class BookSession:
     retry_count: int = 0
 
 class Manager:
-    def __init__(self, env: Literal["dev", "prod"]):
+    def __init__(self, env: Literal["dev", "prod"], max_retry_count: int):
         self.queue = Queue()
         self.workers: list[Task] = []
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
         self.env =  env
+        self.max_retry_count = max_retry_count
         self.storage_dev_path = Path("C:/Users/Isaac/Desktop/me/jobs/filerskeepers/project/storage_dev")
 
-    def cleanup(self, session: BookSession):
+    def cleanup(self, result: Book, sid: int):
         if self.env == "dev":
-            with open(self.storage_dev_path / f"{session.sid}.json", "w") as f:
-                json.dump(session.result.model_dump(), f)
+            with open(self.storage_dev_path / f"{sid}.json", "w") as f:
+                json.dump(result.model_dump(mode="json"), f)
+        self.logger.info(f"[manager] Ready: {sid}")
