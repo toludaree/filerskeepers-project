faec0844c4e5cd3c7ea8f2945123d2fc1d4d4c72
toludaree <isaactoluwani30@gmail.com>
2025-11-26 21:04:20 +0100

    api: implement the first two endpoints

--- bookstoscrape/api/main.py
+++ bookstoscrape/api/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
+from typing import Annotated, Optional
 
 from ..settings import ASYNC_MONGODB_DB, MONGODB_BOOK_COLLECTION
-from .models import GetBooksResponseModel
+from .models import Category, Book, BooksOverview, Rating, SortBy, SortOrder
 
 app = FastAPI()
 
@@ -10,21 +11,84 @@ app = FastAPI()
 async def redirect_to_docs():
     return RedirectResponse(url="/docs")
 
-@app.get("/books", response_model=GetBooksResponseModel)
-async def get_books():
+@app.get("/books", response_model=BooksOverview)
+async def get_books(
+    categories: Annotated[list[Category], Query()] = [],
+    min_price: Annotated[Optional[float], Query()] = None,
+    max_price: Annotated[Optional[float], Query()] = None,
+    ratings: Annotated[list[Rating], Query()] = [],
+    sort_by: Annotated[SortBy, Query()] = "rating",
+    sort_order: Annotated[SortOrder, Query()] = "desc",
+    limit: Annotated[int, Query(gt=0, le=100)] = 20,
+    offset: Annotated[int, Query()] = 0
+):
     """
-    Get all books from the mongodb database
+    Get all books.
 
     Filter by:
         - category
+        - minimum price
+        - maximum price
+        - rating
+
+    Sort by:
+        - rating
+        - price
+        - review count
+
+    Paginate using limit and offset.
     """
     book_collection = ASYNC_MONGODB_DB[MONGODB_BOOK_COLLECTION]
 
+    filters = {}
+    if categories:
+        filters["category"] = {"$in": categories}
+    if any((min_price, max_price)):
+        filters["price_excluding_tax"] = ({"$gte": min_price} if min_price else {}) | ({"$lte": max_price} if max_price else {})
+    if ratings:
+        filters["rating"] = {"$in": ratings}
+
+    # print(f"Categories: {categories}")
+    # print(f"Minimum Price: {min_price}")
+    # print(f"Maximum Price: {max_price}")
+    # print(f"Ratings: {ratings}")
+    # print(filters)
     books = await book_collection.find(
-        {},
-        { "_id": 0, "bts_id": 1, "name": 1, "cover_image_url": 1 }
+        filter=filters,
+        projection={
+            "_id": 0,
+            "bts_id": 1,
+            "name": 1,
+            "category": 1,
+            "price_excluding_tax": 1,
+            "rating": 1,
+            "review_count": 1
+        },
+        sort=[
+            ("price_excluding_tax" if sort_by == "price" else sort_by, -1 if sort_order == "desc" else 1),
+            ("bts_id", 1)
+        ],
+        skip=offset,
+        limit=limit,
     ).to_list()
 
     return {
         "books": books
     }
+
+@app.get("/books/{book_id}", response_model=Book)
+async def get_book(book_id: int):
+    """
+    Given a book ID, return full details about the book
+    """
+    book_collection = ASYNC_MONGODB_DB[MONGODB_BOOK_COLLECTION]
+
+    book: Optional[dict] = await book_collection.find_one(
+        filter={"bts_id": book_id},
+        projection={
+            "_id": 0
+        }
+    )
+    crawl_metadata = book.pop("crawl_metadata")
+    book["source_url"] = crawl_metadata["source_url"]
+    return book
--- bookstoscrape/api/models.py
+++ bookstoscrape/api/models.py
@@ -1,27 +1,50 @@
 from __future__ import annotations
+from enum import IntEnum
+from typing import Literal
 from pydantic import BaseModel
 
 
-class GetBooksResponseModel(BaseModel):
+class BooksOverview(BaseModel):
     books: list[BookOverview]
 
 class BookOverview(BaseModel):
     bts_id: int
     name: str
     category: str
-    price: float
-    rating: float
-    reviews: float
+    price_excluding_tax: float
+    rating: int
+    review_count: float
 
-class BookFull(BaseModel):
+class Book(BaseModel):
     bts_id: int
     name: str
     description: str
     source_url: str
     category: str
-    price: float
+    price_excluding_tax: float
     in_stock: bool
     stock_count: int
-    rating: float
+    rating: int
     review_count: int
     cover_image_url: str
+
+Category = Literal[
+    "Travel", "Mystery", "Historical Fiction", "Sequential Art", "Classics",
+    "Philosophy", "Romance", "Women's Fiction", "Fiction", "Childrens",
+    "Religion", "Nonfiction", "Music", "Default", "Science Fiction",
+    "Sports and Games", "Add a comment", "Fantasy", "New Adult",
+    "Young Adult", "Science", "Poetry", "Paranormal", "Art", "Psychology",
+    "Autobiography", "Parenting", "Adult Fiction", "Humor", "Horror",
+    "History", "Food and Drink", "Christian Fiction", "Business", "Biography",
+    "Thriller", "Contemporary", "Spirituality", "Academic", "Self Help",
+    "Historical", "Christian", "Suspense", "Short Stories", "Novels",
+    "Health", "Politics", "Cultural", "Erotica", "Crime",
+]
+class Rating(IntEnum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+SortBy = Literal["rating", "price", "review_count"]
+SortOrder = Literal["asc", "desc"]
