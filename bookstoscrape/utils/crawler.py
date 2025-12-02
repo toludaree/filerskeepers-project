ce52f08be24c4e1ad724216221d4f20218432a4c
toludaree <isaactoluwani30@gmail.com>
2025-12-02 11:02:46 +0100

    remove unused function

--- bookstoscrape/utils/crawler.py
+++ bookstoscrape/utils/crawler.py
@@ -1,10 +1,6 @@
 import re
-import time
 
 
 def extract_id_from_book_url(url: str):
     book_id = re.search(r"_(\d+)/index\.html$", url).group(1)
     return int(book_id)
-
-def get_milliseconds_since_epoch():
-    return int(time.time() * 1000)
