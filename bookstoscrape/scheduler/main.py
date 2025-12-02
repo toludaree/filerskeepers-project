f9e1456785b17196a9ecf723625e23bf8687d32b
toludaree <isaactoluwani30@gmail.com>
2025-12-02 22:16:24 +0100

    add mongodb job store and cron schedule

--- bookstoscrape/scheduler/main.py
+++ bookstoscrape/scheduler/main.py
@@ -2,12 +2,23 @@ import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.mongodb import MongoDBJobStore
 
+from .. import settings as ss
 from .job import bts_scheduler
 
 
 async def main():
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(bts_scheduler, "interval", seconds=30)
+    scheduler.add_job(
+        bts_scheduler,
+        "cron", hour=12, timezone="UTC",  # Run every day at 12 noon UTC
+        args=["prod"],
+        misfire_grace_time=ss.MISFIRE_GRACE_TIME
+    )
+    store = MongoDBJobStore(
+        database=ss.MONGODB_DB,
+        collection=ss.MONGODB_SCHEDULED_JOBS_COLLECTION
+    )
+    scheduler.add_jobstore(store)
     scheduler.start()
 
     print("Scheduler started. Press CTRL+C to exit.")
