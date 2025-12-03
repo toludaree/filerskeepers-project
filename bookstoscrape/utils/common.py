023b6bfcc3e61e3dc0931ccd898b1aeefc46718a
toludaree <isaactoluwani30@gmail.com>
2025-12-03 16:02:53 +0100

    order category type alphabetically

--- bookstoscrape/utils/common.py
+++ bookstoscrape/utils/common.py
@@ -35,16 +35,16 @@ class Rating(IntEnum):
     FIVE = 5
 
 Category = Literal[
-    "Travel", "Mystery", "Historical Fiction", "Sequential Art", "Classics",
-    "Philosophy", "Romance", "Womens Fiction", "Fiction", "Childrens",
-    "Religion", "Nonfiction", "Music", "Default", "Science Fiction",
-    "Sports and Games", "Add a comment", "Fantasy", "New Adult",
-    "Young Adult", "Science", "Poetry", "Paranormal", "Art", "Psychology",
-    "Autobiography", "Parenting", "Adult Fiction", "Humor", "Horror",
-    "History", "Food and Drink", "Christian Fiction", "Business", "Biography",
-    "Thriller", "Contemporary", "Spirituality", "Academic", "Self Help",
-    "Historical", "Christian", "Suspense", "Short Stories", "Novels",
-    "Health", "Politics", "Cultural", "Erotica", "Crime",
+    "Academic", "Add a comment", "Adult Fiction", "Art", "Autobiography",
+    "Biography", "Business", "Childrens", "Christian", "Christian Fiction",
+    "Classics", "Contemporary", "Crime", "Cultural", "Default", "Erotica",
+    "Fantasy", "Fiction", "Food and Drink", "Health", "Historical",
+    "Historical Fiction", "History", "Horror", "Humor", "Music", "Mystery",
+    "New Adult", "Nonfiction", "Novels", "Paranormal", "Parenting",
+    "Philosophy", "Poetry", "Politics", "Psychology", "Religion", "Romance",
+    "Science", "Science Fiction", "Self Help", "Sequential Art",
+    "Short Stories", "Spirituality", "Sports and Games", "Suspense",
+    "Thriller", "Travel", "Womens Fiction", "Young Adult"
 ]
 
 def setup_logger(
